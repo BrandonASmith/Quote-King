e48bd7153da89dedac426bb82b07032c50560686
Brandon Smith <basmith808@gmail.com>
2016-01-27 11:49:24 -0800

    creates dependencies and server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('May the Force be with You')
+//console.log('May the Force be with You')
+
+var express 	= require('express')
+	bodyParser  = require('body-parser')
+	MongoClient = require('mongodb').MongoClient
+	app			= express()
+
+
+
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
